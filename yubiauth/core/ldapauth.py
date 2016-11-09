9b6c2dd97d2f251a2631cda5f99aeb78d2912c69
Nabil Laadadra <N4BIL@users.noreply.github.com>
2016-11-09 12:07:53 +0100

    Update ldapauth.py
    Password containing é à ù raise an exception: cannot convert UNICODE character to ASCII
    Adding .encode('UTF-8') resolve the probem

--- yubiauth/core/ldapauth.py
+++ yubiauth/core/ldapauth.py
@@ -68,7 +68,7 @@ class LDAPAuthenticator(object):
         try:
             conn = ldap.initialize(self.ldap_server)
             conn.set_option(ldap.OPT_NETWORK_TIMEOUT, 10.0)
-            conn.simple_bind_s(bind_dn, password)
+            conn.simple_bind_s(bind_dn, password.encode('UTF-8'))
             log.info("LDAP authentication successful. "
                      "Bind DN: %s, server: %s", bind_dn, self.ldap_server)
             return (conn, bind_dn)
